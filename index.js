035e49263616603111451715532668d1cb05263f
dujianhao <dujianhao@xiaomi.com>
2021-01-08 16:24:41 +0800

    update: 更新请求参数

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 const converter = require('json-2-csv')
 const getData = require('./request')
 
-const JSON_PATH = 'json'
+const JSON_PATH = 'input'
 const INPUT_PATH = 'input'
 const OUTPUT_PATH = 'output'
 
@@ -18,11 +18,66 @@ if (!fs.existsSync(JSON_PATH)) {
 }
 if (!fs.existsSync(INPUT_PATH)) {
   fs.mkdirSync(INPUT_PATH)
+} else {
+  // fs.rmdirSync(INPUT_PATH, { recursive: true })
+  // fs.mkdirSync(INPUT_PATH)
 }
 if (!fs.existsSync(OUTPUT_PATH)) {
   fs.mkdirSync(OUTPUT_PATH)
+} else {
+  // fs.rmdirSync(OUTPUT_PATH, { recursive: true })
+  // fs.mkdirSync(OUTPUT_PATH)
 }
 
+const tokenList = [
+  'A1wt8IO0b5jGFhvEHzAjJvkiK3EN1QPMgnEU3DZMaMcqgfJnXuXQj9KJ5E2F',
+  'A9SlmMtsd3AePuM85yUrXrEaoxlFLfskOliMRG6kYN_iWXrPlj3Ip4phXMm9',
+  'A0w9ABMEf2hWZmuUr34zdmlyG6F9hfMc8iME7KYcWPeaMeJXzpXAv0I51KX1',
+  'A8S1qFu8R0CujvMMd9M7biGqk0m13et0qgp8lN50UA9SCWq_Bu241_oRTDEt',
+  'AxtqNUhRwFXtSzyhBALcWwJPrHSG8CzBKQjzuQ1J95ox7DWilcC_QjnUg9ge',
+  'A5Pi3ZAJyE0lc4QZzG3kc_qnJBy-SCQ54dFrIUWh77LpxL0KzRi3WvGs-7xW',
+  'Awt6Rdih0CV9mwxxlKjsa7KfnKT2oBwRmbrjyX0Z58qhnCWSBXCvcqmEcxCO',
+  'A4Py7SBZ2B21w5TpXAP0g2r3FEwu-BQJUYNbcbVx3-JZdK36PcinimFc64TG',
+  'A_uKlWjxIPUN6xxBJH78uyIvjNRmUAxhCXTTGe2p1_oRTBVCdSCfohk0Y2j-',
+  'A3MCPbCpKO1FE-S57NmE05oHBHyeqARZwS1LgSUBzxLJJJ2qrXiXutEM26w2',
+  'A-uapfhBMMWdO2wRtASMy1J_fATWAPyxeQbDKV15xyqB_AUy5dCP0onkU8Bu',
+  'A2MSTcD5OL3VY_SJfG-U4wpX9KwOWPSpMf870ZXRv0I51I2aHSiH6kG8y1Sm',
+  'A9uq9QiRAJUti3zhROqcG8KPbDRGsOyB6dCzec0Jt1rxrPXiVYB_AvmUQ_je',
+  'A1MinVBJCI1ls8RZDAWkM7rn5Nx-COT5oYkr4QVhr3KphH1Kjdh3GrFsuxwW',
+  'A-ydIPPkn0i2Rot0D7qTlskSu8EdpZN8kl-kDEY8-Bc6UYI3brVg3-JZdPWV',
+  'A2QVyDuc5yAObhPs19-bjoFKM2lV_YtUSjYctH6U8C_yKQqfpg1Y95ox7GHN',
+  'A9ytcAM07xhGlptEn2Cjpnmiq_GNVYNMAuGUXLbM6EeqAXLn3mVQD1IJZI0F',
+  'A1QlGEvs9_CevmO8Z5Wr3jGaI5nFrXukusgMxO4k4F9i2fpPFr1IJwrh3Ak9',
+  'A6vaZbiBcAXde6xR9OxMixK_PMSWwLxxOc6D6R05h-pBvMVypZBPkkmkEyAu',
+  'A4b3dgUmJWY45PECWauZiJew0XcL58mSnDD-GnC-cqmEcyjh2HcasWy7Tn9D',
+  'AxdmIWz9BJlBH4AVo1sYhx6boIBiXOgdxSxvLWlVM-ZNmDm2cSx7DtUA_-x6',
+  'A4_-ybSVDHGZRwiNa_4gv9bzGCiatOB1fRXn1aGNK_4FcKEeqYRzJo3Yd2iy',
+  'Awd2cfxNFGnRb5DlMxEo144rkLDSDNhNNf5ffdnlIxa9SClm4dxrPkWw78Tq',
+  'A38OGcTlHEEplxhd-0Qwz0YDCFgKZNCl7afX5RFdGy51IJHOGTRjVv2IZzAi',
+  'A9anZjWWlVZoVKHyipPJuKcgIYfbdxkSbJtOCkAuwrlUA3jxqAdqwTxLnlsT',
+  'A28eKVQ1bBG552gtixLAH_ZT-Ih6FMCVXUnHNYHtC17l0IG-ieRThm04V_iS',
+  'A8a3tkXm5Sb4pLHCGh3ZyNdwEbdLJwlC3Ek-WrD-sunEs2ihGLda8az7jtOD',
+  'A18ueeSFfOFJN3j9G5jQL2bj6LjqxLDFzTu3RfG9-45VgHFu-ZRDtt3oRyAC',
+  'A5nor96nIv8Ttf5r0bm-YYwhro5wJo57dz6xc7tfFUA_wrc4A3adqAdqwbBI',
+  'Ay5fbh3u_e7AHAkKcizxMD_4eY_zL_GKBNamEljmmjHsO8D5wL9COdSD9o8r',
+  'A8e2MbyNVKkRr9Alc6nol05rUHCSzJgN9YYfPZml49Z9COmmoZwr_gVwr-Sq',
+  'Ax5vvq0-zb5QbBnaAkaBQK8Iab9j3-H6dISWIsi2imFc67CpMG8yaUQz5teb',
+  'A7fGQczdpHmh_yD1A8f4p_67QKACfIh9ZXgPTQl10wbtuNmWEUwbLnUgnzwa',
+  'Aw5_zj2O3Y7gvGmqkpCRkN9YWe_Tj9Eq5HqGcjhGepHMm6CZoB8imbTj1t8L',
+  'A6fWkVwttEkxTzDFkx2I927LMNByLHit1Sr_nXkFwzZdaMlGgfwLXuXQj3SK',
+  'Ax9uOaTFvCGJd7g9W8CQ7yYjqHiqhHCFjfN3BbF9u04VQDGuuVQDdp2oB8DC',
+  'A5fmoex9hBnBnwCVI6uYB54bIADi3GidRZzvrenVs2bNGLk28az7jlWAf6z6',
+  'A8m4P6638o9jxY77IYyO8Xwx3v4gFr7hJyfhw2svRbDvsueos2bNGLda8R74',
+  'AwNybaDZWJ01QxRp33V0A-p3lMyueJQT0SLb8TXxX2LZ9C16vUgnCuHca4pG',
+  'A7nITz4Hwl_zFZ7LsfaeAexBzi6Qxq7Rl5XRE9v_NeBfYteYIxa9SCcK4WZo',
+]
+
+let tokenIndex = 0
+let token = tokenList[tokenIndex]
+
+const csvResult = {}
+let fileCount = 0
+let walkFinish = false
 
 function walk_dir(JSON_PATH, path, callback) {
   console.log('walk_dir start...')
@@ -54,17 +109,33 @@ function processJson(JSON_PATH, path, fileName) {
 
   let data = fs.readFileSync(JSON_PATH + path, 'utf-8')
 
-  let result = ''
   try {
-    data = JSON.parse(data)
-    result = data.answer.components[0].data.datas
-    converter.json2csv(result, (err, csvData) => {
-      if (err) {
-        return console.log('converter csv error: ' + err)
+    const result = JSON.parse(data)
+    const resultArr = result.data.answer[0].txt[0].content.components[0].data.datas
+    resultArr.forEach(item => {
+      const code = item.code
+      if (!csvResult[code]) {
+        csvResult[code] = []
       }
-      const path = OUTPUT_PATH + '/' + fileName.split('.')[0] + '.csv'
-      writeFile(path, csvData)
-    }, { excelBOM: true })
+      csvResult[code][fileCount] = {
+        '股票代码': item['股票代码'],
+        '股票简称': item['股票简称'],
+        '个股热度': getValueFromKey(item, '个股热度'),
+        '日期': fileName.split('.')[0],
+      }
+    })
+    fileCount++
+    console.log(fileCount, fileName)
+    if (fileCount === 184) {
+      converter.json2csv(Object.values(csvResult).flat(), (err, csvData) => {
+        if (err) {
+          console.log('converter csvResult csv error: ' + err)
+          return
+        }
+        const outputPath = `${OUTPUT_PATH}/csvResult`
+        writeFile(csvData, outputPath, '.csv')
+      }, { excelBOM: true })
+    }
 
   } catch (e) {
     console.log('parse data err: ' + e)
@@ -89,28 +160,43 @@ function writeFile(data, path, extention) {
   })
 }
 
-// walk_dir(JSON_PATH, '', processJson)
 
-function getCsvData({ year, month, day }) {
+function getCsvData({ year, month, day }, token) {
+  const fileName = `${year}_${month}_${day}`
+  const inputPath = INPUT_PATH + '/' + fileName
+  const outputPath = OUTPUT_PATH + '/' + fileName
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(`${inputPath}.json`)) {
+      // console.log('exist ' + `${inputPath}.json`)
+      return resolve()
+    }
+    getData({ year, month, day }, token)
+      .then(result => {
+        writeFile(JSON.stringify(result), inputPath, '.json')
+        const resultArr = result.data.answer[0].txt[0].content.components[0].data.datas
+        // converter.json2csv(resultArr, (err, csvData) => {
+        //   if (err) {
+        //     console.log('converter csv error: ' + err)
+        //     return
+        //   }
+        //   console.log(fileName)
+        //   writeFile(csvData, outputPath, '.csv')
+        // }, { excelBOM: true })
+        return resolve(resultArr)
+      })
+      .catch(e => {
+        console.log('getData err:' + e.message)
+        return reject()
+      })
+  })
+}
 
-  getData({ year, month, day })
-    .then(data => {
-      const fileName = `${year}_${month}_${day}`
-      const inputPath = INPUT_PATH + '/' + fileName
-      const outputPath = OUTPUT_PATH + '/' + fileName
-      writeFile(JSON.stringify(data), inputPath, '.json')
-      const resultArr = data.answer.components[0].data.datas
-      converter.json2csv(resultArr, (err, csvData) => {
-        if (err) {
-          return console.log('converter csv error: ' + err)
-        }
-        console.log(fileName)
-        writeFile(csvData, outputPath, '.csv')
-      }, { excelBOM: true })
-    })
-    .catch(e => {
-      console.log('getData err:' + e.message)
-    })
+function getValueFromKey(obj, keyPrefix) {
+  for (let key in obj) {
+    if (key.startsWith(keyPrefix)) {
+      return obj[key]
+    }
+  }
 }
 
 function traverseDate(startTimeStr, endTimeStr) {
@@ -118,9 +204,15 @@ function traverseDate(startTimeStr, endTimeStr) {
   const endTime = new Date(endTimeStr)
   //遍历日期的方法
   //利用setTime获取两个日期之间差值,差值毫秒换算成天1000*60*60*24
-  const distanceDayLength = (endTime.getTime() - startTime.getTime()) / (1000 * 60 * 60 * 24)
+  const distanceDayLength = (endTime.getTime() - startTime.getTime()) / (1000 * 60 * 60 * 24) + 1
   const startDay = startTime.getDate()
-  for (let i = 0; i <= distanceDayLength; i++) {
+  let count = 0
+  let i = 0
+
+  function getData() {
+    if (i >= distanceDayLength) {
+      return
+    }
     const currentTime = new Date(new Date(startTimeStr).setDate(startDay + i))
 
     const timeData = {
@@ -128,8 +220,31 @@ function traverseDate(startTimeStr, endTimeStr) {
       month: String(currentTime.getMonth() + 1).padStart(2, '0'),
       day: String(currentTime.getDate()).padStart(2, '0'),
     }
-    getCsvData(timeData)
+
+    token = tokenList[tokenIndex]
+    getCsvData(timeData, token)
+      .then(result => {
+        i++
+        getData()
+        if (!result) {
+          return
+        }
+
+        count++
+        console.log(count)
+      })
+      .catch(() => {
+        tokenIndex++
+        if (tokenIndex >= tokenList.length) {
+          console.log('available token out')
+          return
+        }
+        getData()
+      })
   }
+  getData()
 }
 
-traverseDate('2020-07-01', '2021-01-01')
+// traverseDate('2020-07-01', '2020-12-31')
+
+walk_dir(JSON_PATH, '', processJson)
--- request.js
+++ request.js
@@ -7,33 +7,30 @@
 const axios = require('axios')
 const qs = require('qs')
 
-
-module.exports = function getData({ year, month, day }) {
+module.exports = function getData({ year, month, day }, token) {
+  console.log(token)
   const data = qs.stringify({
-    business_cat: 'soniu',
-    codelist: '',
-    comp_id: '5362418',
-    condition: `[{"chunkedResult":"问财关注度, _&_生物医药,_&_${year}年${month}月${day}日","opName":"and","opProperty":"","sonSize":2,"relatedSize":0},{"dateText":"${year}年${month}月${day}日","indexName":"问财关注度","indexProperties":["交易日期 ${year}${month}${day}"],"dateUnit":"日","source":"new_parser","type":"index","indexPropertiesMap":{"交易日期":"${year}${month}${day}"},"reportType":"TRADE_DAILY","dateType":"+区间","valueType":"_浮点型数值(%)","domain":"abs_股票领域","uiText":"${year}年${month}月${day}日的问财关注度","sonSize":0,"queryText":"${year}年${month}月${day}日的问财关注度","relatedSize":0,"tag":"[${year}年${month}月${day}日]问财关注度"},{"indexName":"所属同花顺行业","indexProperties":["概念id 300769","包含T15"],"source":"new_parser","type":"index","indexPropertiesMap":{"概念id":"300769","包含":"T15"},"reportType":"null","valueType":"_所属同花顺行业","domain":"abs_股票领域","uiText":"所属同花顺行业是生物医药","sonSize":0,"queryText":"所属同花顺行业是生物医药","relatedSize":0,"tag":"所属同花顺行业"}]`,
-    'date_range[0]': `${year}${month}${day}`,
-    'iwc_token	': '',
-    'logid': 'b6d0eb93f683b99a5e3af9cd92bbc4c5',
-    'page': '1',
-    'perpage': '1000',
-    'query': `问财关注度, 生物医药，${year}年${month}月${day}日`,
-    'query_type': 'stock',
-    'ret': 'json_all',
-    'sessionid': 'c0849f90057fa6811d1f081cae785fe7',
-    'urp_sort_index': '',
-    'urp_sort_way': '',
-    'urp_use_sort': '1',
-    'user_id': '530945201',
-    'uuid': '24087',
-    'uuids[0]': '24087',
+    question: `科创板，上市天数=537，个股热度，${year}年${month}月${day}日`,
+    perpage: 50,
+    page: 1,
+    log_info: { 'input_type': 'typewrite' },
+    source: 'Ths_iwencai_Xuangu',
+    version: '2.0',
+    add_info: { 'urp': { 'scene': 1, 'company': 1, 'business': 1 }, 'contentType': 'json' },
   })
+  const hexinV = 'AyxdYDOkXwhijEs00JTT1gnS-wFd5dAO0ojkU4ZtOex6ssL3rvWgHyKZtO3V'
   const config = {
     method: 'post',
     url: 'http://www.iwencai.net/unifiedwap/unified-wap/v2/result/get-robot-data',
     headers: {
+      'Pragma': 'no-cache',
+      'Cache-Control': 'no-cache',
+      'hexin-v': token,
+      'Cookie': 'chat_bot_session_id=6215f1df29333f8ef294a0030e2eaa82; other_uid=Ths_iwencai_Xuangu_q9sn32s41xr1kxrxbjjyf0138mxluail; cid=0f17960d1829a1a2a018a566a1cf9aa81610017855; cid=0f17960d1829a1a2a018a566a1cf9aa81610017855; ComputerID=0f17960d1829a1a2a018a566a1cf9aa81610017855; WafStatus=0; PHPSESSID=60706550bb2c6470f6e0f690beca8787; v=A6zd4LMk34jH68u0eFdTVolSe4HdZVLxUg9kvgbquR_6MUJ3LnUgn6IZNHxV',
+      'Connection': 'keep-alive',
+      'Host': 'www.iwencai.com',
+      'Origin': 'http://www.iwencai.com',
+      'Referer': 'http://www.iwencai.com/unifiedwap/result?w=%E7%A7%91%E5%88%9B%E6%9D%BF%EF%BC%8C%E4%B8%8A%E5%B8%82%E5%A4%A9%E6%95%B0%3D537%EF%BC%8C%E9%97%AE%E8%B4%A2%E5%85%B3%E6%B3%A8%E5%BA%A6%EF%BC%8C2020%E5%B9%B41%E6%9C%881%E6%97%A5&querytype=&issugs&sign=1610072782019',
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
       'DNT': '1',
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -48,7 +45,6 @@ module.exports = function getData({ year, month, day }) {
         resolve(response.data)
       })
       .catch(function(error) {
-        console.log(error)
         reject(error)
       })
   })
